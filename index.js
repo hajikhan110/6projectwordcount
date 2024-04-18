024e640a3b08b8cc221cf85fd41b8618889ababa
hajikhan <hajikhan110@yahoo.com>
2024-04-18 12:36:46 +0500

    msg

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+//this module to get data from cmd
+const answers 
+//let answers: any
+//= await inquirer.prompt(
+= await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "enter a sentence:"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+//const array = ['a', 'b','c'];
+console.log(words);
+console.log(`your sentence word count is ${words.length}`);
---
+++ index.ts
@@ -0,0 +1,30 @@
+
+import inquirer from "inquirer";
+//this module to get data from cmd
+
+const answers: {
+sentence: string;
+} 
+
+//let answers: any
+//= await inquirer.prompt(
+
+
+= await inquirer.prompt([
+{
+name: "sentence",
+type: "input",
+message: "enter a sentence:"
+
+
+}
+])
+
+const words = answers.sentence.trim().split(" ")
+
+//const array = ['a', 'b','c'];
+console.log(words);
+
+
+console.log(`your sentence word count is ${words.length}`);
+
